2ba48746e0f0f127e01a88819186b54c33eb4438
Jin Huai Xuan <Jinhuai.Xuan38@myhunter.cuny.edu>
2021-04-20 12:11:38 -0400

    First Commit

---
+++ src/constants.js
@@ -0,0 +1,402 @@
+const Airani_Iofifteen = {
+    name: 'Airani Iofifteen',
+    path: require('./assets/Airani_Iofifteen.png'),
+    twitter: 'airaniiofifteen',
+    youtube: 'https://www.youtube.com/channel/UCAoy6rzhSf4ydcYjJw3WoVg',
+};
+
+const Akai_Haato = {
+    name: 'Akai Haato',
+    path: require('./assets/Akai_Haato.png'),
+    path_gif: require('./assets/Akai_Haato_Animated.gif'),
+    twitter: 'akaihaato',
+    youtube: 'https://www.youtube.com/channel/UC1CfXB_kRs3C-zaeTG3oGyg',
+};
+
+const Aki_Rosenthal = {
+    name: 'Aki Rosenthal',
+    path: require('./assets/Aki_Rosenthal.png'),
+    path_gif: require('./assets/Aki_Rosenthal_Animated.gif'),
+    twitter: 'akirosenthal',
+    youtube: 'https://www.youtube.com/channel/UCFTLzh12_nrtzqBPsTCqenA',
+};
+
+const Amane_Kanata = {
+    name: 'Amane Kanata',
+    path: require('./assets/Amane_Kanata.png'),
+    path_gif: require('./assets/Amane_Kanata_Animated.gif'),
+    twitter: 'amanekanatach',
+    youtube: 'https://www.youtube.com/channel/UCZlDXzGoo7d44bwdNObFacg',
+};
+
+const Aragami_Oga = {
+    name: 'Aragami Oga',
+    path: require('./assets/Aragami_Oga.png'),
+    twitter: 'aragamioga',
+    youtube: 'https://www.youtube.com/channel/UCwL7dgTxKo8Y4RFIKWaf8gA',
+};
+
+const Arurandeisu = {
+    name: 'Arurandeisu',
+    path: require('./assets/Arurandeisu.png'),
+    twitter: 'arurandeisu',
+    youtube: 'https://www.youtube.com/channel/UCKeAhJvy8zgXWbh9duVjIaQ',
+};
+
+const Astel_Leda = {
+    name: 'Astel Leda',
+    path: require('./assets/Astel_Leda.png'),
+    twitter: 'astelleda',
+    youtube: 'https://www.youtube.com/channel/UCNVEsYbiZjH5QLmGeSgTSzg',
+};
+
+const Ayunda_Risu = {
+    name: 'Ayunda Risu',
+    path: require('./assets/Ayunda_Risu.png'),
+    twitter: 'ayunda_risu',
+    youtube: 'https://www.youtube.com/channel/UCOyYb1c43VlX9rc_lT6NKQw',
+};
+
+const AZKi = {
+    name: 'AZKi',
+    path: require('./assets/AZKi.png'),
+    path_gif: require('./assets/AZKi_Animated.gif'),
+    twitter: 'AZKi_VDiVA',
+    youtube: 'https://www.youtube.com/channel/UC0TXe_LYZ4scaW2XMyi5_kw',
+};
+
+const Hanasaki_Miyabi = {
+    name: 'Hanasaki Miyabi',
+    path: require('./assets/Hanasaki_Miyabi.png'),
+    twitter: 'miyabihanasaki',
+    youtube: 'https://www.youtube.com/channel/UC6t3-_N8A6ME1JShZHHqOMw',
+};
+
+const Himemori_Luna = {
+    name: 'Himemori Luna',
+    path: require('./assets/Himemori_Luna.png'),
+    path_gif: require('./assets/Himemori_Luna_Animated.gif'),
+    twitter: 'himemoriluna',
+    youtube: 'https://www.youtube.com/channel/UCa9Y57gfeY0Zro_noHRVrnw',
+};
+
+const Hoshimachi_Suisei = {
+    name: 'Hoshimachi Suisei',
+    path: require('./assets/Hoshimachi_Suisei.png'),
+    path_gif: require('./assets/Hoshimachi_Suisei_Animated.gif'),
+    twitter: 'suisei_hosimati',
+    youtube: 'https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A',
+};
+
+const Hoshinova_Moona = {
+    name: 'Hoshinova Moona',
+    path: require('./assets/Hoshinova_Moona.png'),
+    twitter: 'moonahoshinova',
+    youtube: 'https://www.youtube.com/channel/UCP0BspO_AMEe3aQqqpo89Dg',
+};
+
+const Houshou_Marine = {
+    name: 'Houshou Marine',
+    path: require('./assets/Houshou_Marine.png'),
+    path_gif: require('./assets/Houshou_Marine_Animated.gif'),
+    twitter: 'houshoumarine',
+    youtube: 'https://www.youtube.com/channel/UCCzUftO8KOVkV4wQG1vkUvg',
+};
+
+const Inugami_Korone = {
+    name: 'Inugami Korone',
+    path: require('./assets/Inugami_Korone.png'),
+    path_gif: require('./assets/Inugami_Korone_Animated.gif'),
+    twitter: 'inugamikorone',
+    youtube: 'https://www.youtube.com/channel/UChAnqc_AY5_I3Px5dig3X1Q',
+};
+
+const Kagami_Kira = {
+    name: 'Kagami Kira',
+    path: require('./assets/Kagami_Kira.png'),
+    twitter: 'kagamikirach',
+    youtube: 'https://www.youtube.com/channel/UCEzsociuFqVwgZuMaZqaCsg',
+};
+
+const Kageyama_Shien = {
+    name: 'Kageyama Shien',
+    path: require('./assets/Kageyama_Shien.png'),
+    twitter: 'kageyamashien',
+    youtube: 'https://www.youtube.com/channel/UChSvpZYRPh0FvG4SJGSga3g',
+};
+
+const Kanade_Izuru = {
+    name: 'Kanade Izuru',
+    path: require('./assets/Kanade_Izuru.png'),
+    twitter: 'kanadeizuru',
+    youtube: 'https://www.youtube.com/channel/UCZgOv3YDEs-ZnZWDYVwJdmA',
+};
+
+const Kiryu_Coco = {
+    name: 'Kiryu Coco',
+    path: require('./assets/Kiryu_Coco.png'),
+    path_gif: require('./assets/Kiryu_Coco_Animated.gif'),
+    twitter: 'kiryucoco',
+    youtube: 'https://www.youtube.com/channel/UCS9uQI-jC3DE0L4IpXyvr6w',
+};
+
+const Kishido_Temma = {
+    name: 'Kishido Temma',
+    path: require('./assets/Kishido_Temma.png'),
+    twitter: 'kishidotemma',
+    youtube: 'https://www.youtube.com/channel/UCGNI4MENvnsymYjKiZwv9eg',
+};
+
+
+const Minato_Aqua = {
+    name: 'Minato Aqua',
+    path: require('./assets/Minato_Aqua.png'),
+    path_gif: require('./assets/Minato_Aqua_Animated.gif'),
+    twitter: 'minatoaqua',
+    youtube: 'https://www.youtube.com/channel/UC1opHUrw8rvnsadT-iGp7Cg',
+};
+
+const Murasaki_Shion = {
+    name: 'Murasaki Shion',
+    path: require('./assets/Murasaki_Shion.png'),
+    path_gif: require('./assets/Murasaki_Shion_Animated.gif'),
+    twitter: 'murasakishionch',
+    youtube: 'https://www.youtube.com/channel/UCXTpFs_3PqI41qX2d9tL2Rw',
+};
+
+const Nakiri_Ayame = {
+    name: 'Nakiri Ayame',
+    path: require('./assets/Nakiri_Ayame.png'),
+    path_gif: require('./assets/Nakiri_Ayame_Animated.gif'),
+    twitter: 'nakiriayame',
+    youtube: 'https://www.youtube.com/channel/UC7fk0CB07ly8oSl0aqKkqFg',
+};
+
+const Natsuiro_Matsuri = {
+    name: 'Natsuiro Matsuri',
+    path: require('./assets/Natsuiro_Matsuri.png'),
+    path_gif: require('./assets/Natsuiro_Matsuri_Animated.gif'),
+    twitter: 'natsuiromatsuri',
+    youtube: 'https://www.youtube.com/channel/UCQ0UDLQCjY0rmuxCDE38FGg',
+};
+
+const Nekomata_Okayu = {
+    name: 'Nekomata Okayu',
+    path: require('./assets/Nekomata_Okayu.png'),
+    path_gif: require('./assets/Nekomata_Okayu_Animated.gif'),
+    twitter: 'nekomataokayu',
+    youtube: 'https://www.youtube.com/channel/UCvaTdHTWBGv3MKj3KVqJVCw',
+};
+
+const Ookami_Mio = {
+    name: 'Ookami Mio',
+    path: require('./assets/Ookami_Mio.png'),
+    path_gif: require('./assets/Ookami_Mio_Animated.gif'),
+    twitter: 'ookamimio',
+    youtube: 'https://www.youtube.com/channel/UCp-5t9SrOQwXMU7iIjQfARg',
+};
+
+const Oozora_Subaru = {
+    name: 'Oozora Subaru',
+    path: require('./assets/Oozora_Subaru.png'),
+    path_gif: require('./assets/Oozora_Subaru_Animated.gif'),
+    twitter: 'oozorasubaru',
+    youtube: 'https://www.youtube.com/channel/UCvzGlP9oQwU--Y0r9id_jnA',
+};
+
+const Rikka = {
+    name: 'Rikka',
+    path: require('./assets/Rikka.png'),
+    twitter: 'rikkaroid',
+    youtube: 'https://www.youtube.com/channel/UC9mf_ZVpouoILRY9NUIaK-w',
+};
+
+const Roboco = {
+    name: 'Roboco',
+    path: require('./assets/Roboco.png'),
+    path_gif: require('./assets/Roboco_Animated.gif'),
+    twitter: 'robocosan',
+    youtube: 'https://www.youtube.com/channel/UCDqI2jOz0weumE8s7paEk6g',
+};
+
+const Sakura_Miko = {
+    name: 'Sakura Miko',
+    path: require('./assets/Sakura_Miko.png'),
+    path_gif: require('./assets/Sakura_Miko_Animated.gif'),
+    twitter: 'sakuramiko35',
+    youtube: 'https://www.youtube.com/channel/UC-hM6YJuNYVAmUWxeIr9FeA',
+};
+
+const Shirakami_Fubuki = {
+    name: 'Shirakami Fubuki',
+    path: require('./assets/Shirakami_Fubuki.png'),
+    path_gif: require('./assets/Shirakami_Fubuki_Animated.gif'),
+    twitter: 'shirakamifubuki',
+    youtube: 'https://www.youtube.com/channel/UCdn5BQ06XqgXoAxIhbqw5Rg',
+};
+
+const Shiranui_Flare = {
+    name: 'Shiranui Flare',
+    path: require('./assets/Shiranui_Flare.png'),
+    path_gif: require('./assets/Shiranui_Flare_Animated.gif'),
+    twitter: 'shiranuiflare',
+    youtube: 'https://www.youtube.com/channel/UCvInZx9h3jC2JzsIzoOebWg',
+};
+
+const Shirogane_Noel = {
+    name: 'Shirogane Noel',
+    path: require('./assets/Shirogane_Noel.png'),
+    path_gif: require('./assets/Shirogane_Noel_Animated.gif'),
+    twitter: 'shiroganenoel',
+    youtube: 'https://www.youtube.com/channel/UCdyqAaZDKHXg4Ahi7VENThQ',
+};
+
+const Tokino_Sora = {
+    name: 'Tokino Sora',
+    path: require('./assets/Tokino_Sora.png'),
+    path_gif: require('./assets/Tokino_Sora_Animated.gif'),
+    twitter: 'tokino_sora',
+    youtube: 'https://www.youtube.com/channel/UCp6993wxpyDPHUpavwDFqgg',
+};
+
+const Tokoyami_Towa = {
+    name: 'Tokoyami Towa',
+    path: require('./assets/Tokoyami_Towa.png'),
+    path_gif: require('./assets/Tokoyami_Towa_Animated.gif'),
+    twitter: 'tokoyamitowa',
+    youtube: 'https://www.youtube.com/channel/UC1uv2Oq6kNxgATlCiez59hw',
+};
+
+const Tsukishita_Kaoru = {
+    name: 'Tsukishita Kaoru',
+    path: require('./assets/Tsukishita_Kaoru.png'),
+    twitter: 'tsukishitakaoru',
+    youtube: 'https://www.youtube.com/channel/UCsehvfwaWF6nWuFnXI0AqZQ',
+};
+
+const Tsunomaki_Watame = {
+    name: 'Tsunomaki Watame',
+    path: require('./assets/Tsunomaki_Watame.png'),
+    path_gif: require('./assets/Tsunomaki_Watame_Animated.gif'),
+    twitter: 'tsunomakiwatame',
+    youtube: 'https://www.youtube.com/channel/UCqm3BQLlJfvkTsX_hvm0UmA',
+};
+
+const Uruha_Rushia = {
+    name: 'Uruha Rushia',
+    path: require('./assets/Uruha_Rushia.png'),
+    path_gif: require('./assets/Uruha_Rushia_Animated.gif'),
+    twitter: 'uruharushia',
+    youtube: 'https://www.youtube.com/channel/UCl_gCybOJRIgOXw6Qb4qJzQ',
+};
+
+const Usada_Pekora = {
+    name: 'Usada Pekora',
+    path: require('./assets/Usada_Pekora.png'),
+    path_gif: require('./assets/Usada_Pekora_Animated.gif'),
+    twitter: 'usadapekora',
+    youtube: 'https://www.youtube.com/channel/UC1DCedRgGHBdm81E1llLhOQ',
+};
+
+const Yozora_Mel = {
+    name: 'Yozora Mel',
+    path: require('./assets/Yozora_Mel.png'),
+    path_gif: require('./assets/Yozora_Mel_Animated.gif'),
+    twitter: 'yozoramel',
+    youtube: 'https://www.youtube.com/channel/UCD8HOxPs4Xvsm8H0ZxXGiBw',
+};
+
+const Yukoku_Roberu = {
+    name: 'Yukoku Roberu',
+    path: require('./assets/Yukoku_Roberu.png'),
+    twitter: 'yukokuroberu',
+    youtube: 'https://www.youtube.com/channel/UCANDOlYTJT7N5jlRC3zfzVA',
+};
+
+const Yuzuki_Choco = {
+    name: 'Yuzuki Choco',
+    path: require('./assets/Yuzuki_Choco.png'),
+    path_gif: require('./assets/Yuzuki_Choco_Animated.gif'),
+    twitter: 'yuzukichococh',
+    youtube: 'https://www.youtube.com/channel/UC1suqwovbL1kzsoaZgFZLKg',
+};
+
+const hololivejp_assets = [];
+const hololiveid_assets = [];
+const holostar_assets = [];
+
+// Hololive JP ************************************
+
+// Zero Generation
+hololivejp_assets.push(AZKi);
+hololivejp_assets.push(Hoshimachi_Suisei);
+hololivejp_assets.push(Roboco);
+hololivejp_assets.push(Sakura_Miko);
+hololivejp_assets.push(Tokino_Sora);
+
+// First Generation
+hololivejp_assets.push(Akai_Haato);
+hololivejp_assets.push(Aki_Rosenthal);
+hololivejp_assets.push(Natsuiro_Matsuri);
+hololivejp_assets.push(Yozora_Mel);
+
+// Second Generation
+hololivejp_assets.push(Minato_Aqua);
+hololivejp_assets.push(Murasaki_Shion);
+hololivejp_assets.push(Nakiri_Ayame);
+hololivejp_assets.push(Oozora_Subaru);
+hololivejp_assets.push(Yuzuki_Choco);
+
+// Hololive Gamers
+hololivejp_assets.push(Inugami_Korone);
+hololivejp_assets.push(Nekomata_Okayu);
+hololivejp_assets.push(Ookami_Mio);
+hololivejp_assets.push(Shirakami_Fubuki);
+
+// Third Generation
+hololivejp_assets.push(Houshou_Marine);
+hololivejp_assets.push(Shiranui_Flare);
+hololivejp_assets.push(Shirogane_Noel);
+hololivejp_assets.push(Uruha_Rushia);
+hololivejp_assets.push(Usada_Pekora);
+
+// Fourth Generation
+hololivejp_assets.push(Amane_Kanata);
+hololivejp_assets.push(Himemori_Luna);
+hololivejp_assets.push(Kiryu_Coco);
+hololivejp_assets.push(Tokoyami_Towa);
+hololivejp_assets.push(Tsunomaki_Watame);
+
+// Hololive Indonesia ************************************
+hololiveid_assets.push(Airani_Iofifteen);
+hololiveid_assets.push(Ayunda_Risu);
+hololiveid_assets.push(Hoshinova_Moona);
+
+// Holostar ************************************
+holostar_assets.push(Aragami_Oga);
+holostar_assets.push(Arurandeisu);
+holostar_assets.push(Astel_Leda);
+holostar_assets.push(Hanasaki_Miyabi);
+holostar_assets.push(Kagami_Kira);
+holostar_assets.push(Kageyama_Shien);
+holostar_assets.push(Kanade_Izuru);
+holostar_assets.push(Kishido_Temma);
+holostar_assets.push(Rikka);
+holostar_assets.push(Tsukishita_Kaoru);
+holostar_assets.push(Yukoku_Roberu);
+
+const getHololiveJPAssets = () => {
+    return hololivejp_assets;
+};
+
+const getHololiveIDAssets = () => {
+    return hololiveid_assets;
+};
+
+const getHolostarAssets = () => {
+    return holostar_assets;
+}
+
+exports.getHololiveJPAssets = getHololiveJPAssets;
+exports.getHololiveIDAssets = getHololiveIDAssets;
+exports.getHolostarAssets = getHolostarAssets;
---
+++ src/index.js
@@ -0,0 +1,271 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {getHololiveJPAssets, getHololiveIDAssets, getHolostarAssets } from './constants.js';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+const hololivejp_assets = getHololiveJPAssets();
+const hololiveid_assets = getHololiveIDAssets();
+const holostar_assets = getHolostarAssets();
+
+const next_page = require('./assets/right_arrow.png'); 
+const prev_page = require('./assets/left_arrow.png');
+const background_image = require('./assets/background.png');
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hololivejp: hololivejp_assets,
+            hololiveid: hololiveid_assets,
+            holostar: holostar_assets,
+            active_list: 'hololive jp',
+            active_page: hololivejp_assets[0].name,
+            index: 0,
+            page_number: 1,
+            last_page: false,
+        };
+    };
+
+    setActiveList(value, assets) {
+        this.setState({
+            active_list: value,
+            active_page: assets[0].name,
+            index: 0,
+            page_number: 1,
+            last_page: false,
+        });
+    }
+
+    changeActiveList(value) {
+        if (this.state.active_list !== 'hololive jp' && value === 'hololive jp') {
+            this.setActiveList(value, this.state.hololivejp);
+        }
+        else if (this.state.active_list !== 'hololive id' && value === 'hololive id') {
+            this.setActiveList(value, this.state.hololiveid);
+        }
+        else if (this.state.active_list !== 'holostar' && value === 'holostar') {
+            this.setActiveList(value, this.state.holostar);
+        }
+    };
+
+    createList(assets) {
+        const begin_index = (this.state.page_number - 1) * 7;
+        return (
+            <ul id = 'vtuber-list'>
+                {assets.map((idol, list_index) => {
+                    if (list_index >= begin_index && list_index < (begin_index + 7)) {
+                        return (
+                            <li className = {this.state.index === list_index ? 'active-page' : 'inactive-page'} key = {list_index} onClick = {() => {
+                                this.setState({ 
+                                    index: list_index,
+                                });
+                            }}>
+                                <div className = 'vtuber-icon' style = {{backgroundImage: `url(${idol.path})`}}>
+                                    <div className = 'vtuber-icon-overlay-gif'>
+                                        <div className = 'vtuber-icon-hover-gif'>
+                                            {idol.path_gif ? <img className = 'vtuber-gif' src = {idol.path_gif} alt = {idol.name} /> : console.log()}
+                                        </div>
+                                    </div>
+                                    <div className = 'vtuber-icon-overlay-text'>
+                                        <div className = 'vtuber-icon-text'>
+                                            {idol.name}
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    }
+                    else {
+                        return console.log();
+                    }
+                })}
+            </ul>
+        );
+    }
+
+    renderList() {
+        if (this.state.active_list === 'hololive jp') {
+            return (
+                this.createList(this.state.hololivejp)
+            );
+        }
+        else if (this.state.active_list === 'hololive id') {
+            return (
+                this.createList(this.state.hololiveid)
+            );
+        }
+        else {
+            return (
+                this.createList(this.state.holostar)
+            );
+        }
+    }
+
+    nextPageHelper (value, assets) {
+        if (this.state.page_number < Math.ceil(assets.length / 7)) {
+            const page_number_copy = this.state.page_number + 1;
+            const new_index = this.state.page_number * 7;
+            if (page_number_copy === Math.ceil(assets.length / 7)) {
+                this.setState({
+                    index: new_index,
+                    page_number: page_number_copy,
+                    last_page: true,
+                });
+            }
+            else {
+                this.setState({
+                    index: new_index,
+                    page_number: page_number_copy,
+                    last_page: false,
+                });
+            }
+        }
+    }
+
+    createYoutubeLivestreamEmbed () {
+        return (
+            <div className = 'youtube-player'>
+                
+            </div>
+        );
+    }
+
+    createTwitterEmbed (name) {
+        return (
+            <div className = 'twitter-embed-div'>
+                <TwitterTimelineEmbed key = {name} sourceType = 'profile' screenName = {name} options = {{width: 300, height: 400}} />
+            </div>
+        );
+    }
+
+    renderPages() {
+        if (this.state.active_list === 'hololive jp') {
+            return (
+                <div className = 'idol-page'>
+                    {this.createTwitterEmbed(this.state.hololivejp[this.state.index].twitter)}
+                    {this.createYoutubeLivestreamEmbed()}
+                </div>
+            );
+        }
+        else if (this.state.active_list === 'hololive id') {
+            return (
+                <div className = 'idol-page'>
+                    {this.createTwitterEmbed(this.state.hololiveid[this.state.index].twitter)}
+                    {this.createYoutubeLivestreamEmbed()}
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className = 'idol-page'>
+                    {this.createTwitterEmbed(this.state.holostar[this.state.index].twitter)}
+                    {this.createYoutubeLivestreamEmbed()}
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className = 'scroll-container'>
+                <div className = 'navigation-bar'>
+                    <div className = 'group-selector'>
+                        <label id = {this.state.active_list === 'hololive jp' ? 'hololivejp-group-active' : 'hololivejp-group'}>
+                            <input type = 'radio' name = 'option-radio' onClick = {() => {
+                                if (this.state.active_list !== 'hololive jp') {
+                                    this.changeActiveList('hololive jp');
+                                }
+                            }} defaultChecked/>
+                            <span className = 'input-radio'/>
+                            HOLOLIVE JP
+                        </label>
+                        <label id = {this.state.active_list === 'hololive id' ? 'hololiveid-group-active' : 'hololiveid-group'}>
+                            <input type = 'radio' name = 'option-radio' onClick = {() => {
+                                if (this.state.active_list !== 'hololive id') {
+                                    this.changeActiveList('hololive id');
+                                }
+                            }} />
+                            <span className = 'input-radio'/>
+                            HOLOLIVE ID
+                        </label>
+                        <label id = {this.state.active_list === 'holostar' ? 'holostar-group-active' : 'holostar-group'}>
+                            <input type = 'radio' name = 'option-radio' onClick = {() => {
+                                if (this.state.active_list !== 'holostar') {
+                                    this.changeActiveList('holostar');
+                                    
+                                }
+                            }} />
+                            <span className = 'input-radio'/>
+                            HOLOSTAR
+                        </label>
+                    </div>
+                    <div className = 'list-wrapper'>
+                        <div className = 'prev-page-icon'>
+                            <button className = 'prev-page-button' onClick = {() => {
+                                    if (this.state.page_number > 1) {
+                                        const page_number_copy = this.state.page_number - 1;
+                                        const new_index = (page_number_copy - 1) * 7;
+                                        this.setState({
+                                            index: new_index,
+                                            page_number: page_number_copy,
+                                            last_page: false,
+                                        });
+                                    }
+                                }} disabled = {this.state.page_number === 1}>
+                                    <img id = {this.state.page_number === 1 ? 'prev-page-button-inactive' : 'prev-page-button-active'} src = {prev_page} alt="Previous Page" />
+                            </button>
+                        </div>
+                        <div className = 'list-window'>
+                            {this.renderList()}
+                        </div>
+                        <div className = 'next-page-icon'>
+                            <button className = 'next-page-button' onClick = {() => {
+                                if (this.state.active_list === 'hololive jp') {
+                                    this.nextPageHelper('hololive jp', this.state.hololivejp);
+                                }
+                                else if (this.state.active_list === 'hololive id') {
+                                    this.nextPageHelper('hololive id', this.state.hololiveid);
+                                }
+                                else {
+                                    this.nextPageHelper('holostar', this.state.holostar);
+                                }
+                            }} disabled = {this.state.last_page || (this.state.active_list === 'hololive id' && this.state.page_number === 1)}>
+                                <img id = {this.state.last_page || (this.state.active_list === 'hololive id' && this.state.page_number === 1) ? 'next-page-button-inactive' : 'next-page-button-active'} src = {next_page} alt="Next Page" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className = 'fullpage-container'>
+                    <div className = 'fullpage-background' style = {{backgroundImage: `url(${background_image})`}}>
+                    </div>
+                    <div className = 'page-background'>
+                    </div>
+                    <div className = 'page-wrapper'>
+                        {this.renderPages()}
+                    </div>
+                </div>
+            </div>
+        );
+    };
+};
+
+class Footer extends React.Component {
+    render () {
+        return (
+            <div className = 'footer'>
+                <p className = 'footer-text'>Disclaimer: PekoPeko is a fansite. I do not own any of the assets or characters used.</p>
+            </div>
+        );
+    };
+};
+  
+ReactDOM.render(
+    <Main />,
+    document.getElementById('main')
+);
+
+ReactDOM.render(
+    <Footer />,
+    document.getElementById('footer')
+);
